9b673629a3fd9151fc72d3ce9d574f56b2d4cd3d
최영창 <choiyc1446@gmail.com>
2023-07-20 16:23:35 +0900

    Refactor : feed Dto 이름 변경, 알림 조회 추가

--- ourola/backend/src/main/java/com/mk/ourola/api/user/service/FanUserService.java
+++ ourola/backend/src/main/java/com/mk/ourola/api/user/service/FanUserService.java
@@ -1,37 +1,13 @@
 package com.mk.ourola.api.user.service;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+import java.util.List;
 
-import com.mk.ourola.api.user.repository.FanUserRepository;
-import com.mk.ourola.api.user.repository.dto.FanUserDto;
 import com.mk.ourola.api.user.repository.dto.FanUserSignUpDto;
+import com.mk.ourola.api.user.repository.dto.NotificationDto;
 
-import lombok.RequiredArgsConstructor;
+public interface FanUserService {
 
-@Service
-@RequiredArgsConstructor
-public class FanUserService {
+	public void signUp(FanUserSignUpDto userSignUpDto) throws Exception;
 
-    private final FanUserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    public void signUp(FanUserSignUpDto userSignUpDto) throws Exception {
-
-        if (userRepository.findByEmail(userSignUpDto.getEmail()).isPresent()) {
-            throw new Exception("이미 존재하는 이메일입니다.");
-        }
-
-        FanUserDto user = FanUserDto.builder()
-                .email(userSignUpDto.getEmail())
-                .password(userSignUpDto.getPassword())
-                .name(userSignUpDto.getName())
-                .age(userSignUpDto.getAge())
-                .tel(userSignUpDto.getTel())
-//                .role(Role.USER)
-                .build();
-
-        user.passwordEncode(passwordEncoder);
-        userRepository.save(user);
-    }
+	public List<NotificationDto> getNotification(String email);
 }
---
+++ ourola/backend/src/main/java/com/mk/ourola/api/user/service/FanUserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.mk.ourola.api.user.service;
+
+import java.util.List;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.mk.ourola.api.user.repository.FanUserRepository;
+import com.mk.ourola.api.user.repository.NotificationRepository;
+import com.mk.ourola.api.user.repository.dto.FanUserDto;
+import com.mk.ourola.api.user.repository.dto.FanUserSignUpDto;
+import com.mk.ourola.api.user.repository.dto.NotificationDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FanUserServiceImpl implements FanUserService {
+
+	private final FanUserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final NotificationRepository notificationRepository;
+
+	public void signUp(FanUserSignUpDto userSignUpDto) throws Exception {
+
+		if (userRepository.findByEmail(userSignUpDto.getEmail()).isPresent()) {
+			throw new Exception("이미 존재하는 이메일입니다.");
+		}
+
+		FanUserDto user = FanUserDto.builder()
+			.email(userSignUpDto.getEmail())
+			.password(userSignUpDto.getPassword())
+			.name(userSignUpDto.getName())
+			.age(userSignUpDto.getAge())
+			.tel(userSignUpDto.getTel())
+			//                .role(Role.USER)
+			.build();
+
+		user.passwordEncode(passwordEncoder);
+		userRepository.save(user);
+	}
+
+	public List<NotificationDto> getNotification(String email) {
+		int loginUserId = userRepository.findByEmail(email).get().getId();
+		return notificationRepository.findByFanUserDto_Id(loginUserId);
+	}
+}
