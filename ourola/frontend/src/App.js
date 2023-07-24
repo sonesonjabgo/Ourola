e2d30ac915cf4c00538e8a507fba8154f98aa927
ChangHo_Jin <65750442+jin-chang-ho@users.noreply.github.com>
2023-07-25 02:14:51 +0900

    Feat: 공지사항 목록 기능 추가 (FE)

--- ourola/frontend/src/App.js
+++ ourola/frontend/src/App.js
@@ -1,30 +1,15 @@
-import AnnouncementList from "./components/announcement/AnnouncementList";
+import { Link } from "react-router-dom";
 
 function App() {
-  const dummyAnnouncement = [
-    {
-      id: 1,
-      title: "테스트 1",
-      content: "예쁜 프론트 기원",
-      createTime: "2023-07-22",
-    },
-    {
-      id: 2,
-      title: "테스트 2",
-      content: "예쁜 프론트 기원2",
-      createTime: "2023-07-23",
-    },
-    {
-      id: 3,
-      title: "테스트 3",
-      content: "예쁜 프론트 기원3",
-      createTime: "2023-07-24",
-    },
-  ];
+  const artist = "BTS";
 
   return (
-    <div className="App">
-      <AnnouncementList announcementList={dummyAnnouncement}></AnnouncementList>
+    <div id="App" className="App">
+      {/* <Header></Header> 아마 전체 페이지에 다 들어가지 않을까?*/}
+      <Link to="/announcement" state={artist}>
+        공지사항
+      </Link>
+      {/* <Footer></Footer> 아마 전체 페이지에 다 들어가지 않을까?*/}
     </div>
   );
 }
--- ourola/frontend/src/index.js
+++ ourola/frontend/src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import App from "./App";
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Announcement from "./components/announcement/Announcement";
 
 const container = document.getElementById("root");
-render(<App />, container);
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/announcement" element={<Announcement />}></Route>
+    </Routes>
+  </BrowserRouter>,
+  container
+);
