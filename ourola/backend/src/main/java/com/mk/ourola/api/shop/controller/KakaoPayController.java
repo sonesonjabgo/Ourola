7ce0094c2193a338e6d3bb6243ba8e2cf2bb8290
김경숙 <lovablekks@naver.com>
2023-07-25 10:52:56 +0900

    Feat: 카카오페이 결제 시스템 구현

--- ourola/backend/src/main/java/com/mk/ourola/api/shop/controller/KakaoPayController.java
+++ ourola/backend/src/main/java/com/mk/ourola/api/shop/controller/KakaoPayController.java
@@ -1,24 +1,44 @@
 package com.mk.ourola.api.shop.controller;
 
-import org.apache.coyote.Response;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mk.ourola.api.shop.repository.dto.KakaoPayReadyVO;
 import com.mk.ourola.api.shop.service.KakaoPayServiceImpl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 
 @RestController
 @RequestMapping("/payment")
 @RequiredArgsConstructor
+@Log
 public class KakaoPayController {
 	private final KakaoPayServiceImpl kakaoPayService;
 
+	@GetMapping("/ready")
+	public void kakaoPayGet() {
+
+	}
+
 	// 결제 요청
 	@PostMapping("/ready")
-	public ResponseEntity readyToKakaoPay() {
-		return kakaoPayService.kakaoPayReady();
+	public String kakaoPayReady() {
+		log.info("===============kakaoPay post===============");
+		return "redirect:" + kakaoPayService.kakaoPayReady();
+	}
+
+	// 결제 진행 중 취소
+	@GetMapping("/success")
+	public void kakaoPaySuccess(@RequestParam("pg_token") String pg_token, Model model) {
+		log.info("kakaoPaySuccess get......................");
+		log.info("kakaoPaySuccess pg_token : " + pg_token);
+
+		model.addAttribute("info", kakaoPayService.kakaoPayInfo(pg_token));
+		log.info("" + kakaoPayService.kakaoPayInfo(pg_token).getAmount());
 	}
 }
