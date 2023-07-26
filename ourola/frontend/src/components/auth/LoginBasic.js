73dc7f54ae9a1ccb9c0e4fc1015f198c3b2d4a97
Jaehyung <thswogud02@gmail.com>
2023-07-26 15:37:58 +0900

    Feat: 로그인 모달 디자인 수정 및 로그인 요청 보내기

--- ourola/frontend/src/components/auth/LoginBasic.js
+++ ourola/frontend/src/components/auth/LoginBasic.js
@@ -1,58 +1,82 @@
+import { useState } from 'react';
+import axios from 'axios';
 import styles from '../../style/auth/ModalBasic.module.css';
-// import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { loginUser } from '../../../_actions/user_action';
 
-function LoginBasic({setModalOpen}) {
-    // 모달 끄기 
-    const closeModal = () => {
-        setModalOpen(false);
+function LoginBasic({ setModalOpen }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // 모달 끄기
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  // Form submit handler to send login data to the backend
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // 이메일과 비밀번호를 담은 데이터 객체 생성
+    const data = {
+      email: email,
+      password: password,
     };
 
-    // const dispatch = useDispatch();
+    try {
+      // 로그인 데이터를 백엔드로 POST 요청 전송
+      const response = await axios.post('http://localhost:8000/login', data);
 
-    // const [Email, setEmail] = useState("");
-    // const [Password, setPassword] = useState("");
+      // 서버로부터 응답 처리
+      if (response.status === 200) {
+        // 로그인 성공 시 필요한 작업 수행
+        // 예를 들면 로그인 상태 관리, 페이지 이동 등
+        console.log(response.headers.get("Authorization"))
 
-    // const onEmailHandler = (event) => {
-    //     setEmail(event.currentTarget.value);
-    // }
-    // const onPasswordHandler = (event) => {
-    //     setPassword(event.currentTarget.value);
-    // }
+      } else {
+        // 로그인 실패 시 필요한 작업 수행
+        // 예를 들면 에러 메시지 표시 등
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
-    // const onSubmitHandler = (event) => {
-    //     // 버튼만 누르면 리로드 되는것을 막아줌
-    //     event.preventDefault();
+  return (
+    <div className={styles.container}>
+      <button className={styles.close} onClick={closeModal}>
+        X
+      </button>
+      <p className={styles.logintitle}>로그인</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='email'
+          className={styles.inputbox}
+          placeholder='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='password'
+          className={styles.inputbox}
+          placeholder='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-    //     console.log('Email', Email);
-    //     console.log('Password', Password);
-        
-    //     let body = {
-    //         email: Email,
-    //         password: Password,
-    //     }
+        {/* <div className={styles.checkbox}>
+            <label>
+                <input type="checkbox" name="option1" value="value1"/> 아이디 저장
+            </label>
+            <label>
+                <input type="checkbox" name="option1" value="value1"/> 자동 로그인
+            </label>
+        </div> */}
 
-    //     dispatch(loginUser(body));
-    // }
-
-
-    return (
-        <div className={styles.container}>
-            <button className={styles.close} onClick={closeModal}>X</button>
-            {/* <form style={{ display: 'flex', flexDirection: 'column'}}
-                onSubmit={onSubmitHandler}
-            >
-                <label>Email</label>
-                <input type='email' value={Email} onChange={onEmailHandler}/>
-                <label>Password</label>
-                <input type='password' value={Password} onChange={onPasswordHandler}/>
-                <br />
-                <button formAction=''>
-                    Login
-                </button>
-            </form> */}
-        </div>
-    );
+        <button type='submit' className={styles.loginsubmitbutton}>
+          로그인
+        </button>
+      </form>
+    </div>
+  );
 }
+
 export default LoginBasic;
