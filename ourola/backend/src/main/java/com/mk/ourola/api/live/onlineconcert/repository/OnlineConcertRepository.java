e0c6a4078133b593908f43a4e38cf86e3fb6d483
최가연 <cn01803@naver.com>
2023-07-27 14:57:37 +0900

    Feat: shop 페이지 상품 CRUD

--- ourola/backend/src/main/java/com/mk/ourola/api/live/onlineconcert/repository/OnlineConcertRepository.java
+++ ourola/backend/src/main/java/com/mk/ourola/api/live/onlineconcert/repository/OnlineConcertRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.mk.ourola.api.live.onlineconcert.repository.dto.OnlineConcertDto;
 
 public interface OnlineConcertRepository extends JpaRepository<OnlineConcertDto, Integer> {
-	List<OnlineConcertDto> findById(int concertId);
+	OnlineConcertDto findById(int concertId);
 
 	List<OnlineConcertDto> findByGroupChannelDto_Id(int groupId);
+
 }
--- ourola/backend/src/main/java/com/mk/ourola/api/live/onlineconcert/repository/dto/OnlineConcertDto.java
+++ ourola/backend/src/main/java/com/mk/ourola/api/live/onlineconcert/repository/dto/OnlineConcertDto.java
@@ -10,8 +10,13 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mk.ourola.api.artist.repository.dto.GroupChannelDto;
 
 import lombok.Getter;
@@ -24,7 +29,7 @@ import lombok.ToString;
 @Setter
 @ToString
 @NoArgsConstructor
-@DynamicInsert
+@DynamicInsert @DynamicUpdate
 public class OnlineConcertDto {
 
 	@Id
@@ -39,12 +44,17 @@ public class OnlineConcertDto {
 
 	private String content;
 
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
 	@Column(name = "start_time")
 	private Date startTime;
 
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
 	@Column(name = "ticketing_time")
 	private Date ticketingTime;
 
+	@CreationTimestamp
 	@Column(name = "create_date")
 	private Date createDate;
 
