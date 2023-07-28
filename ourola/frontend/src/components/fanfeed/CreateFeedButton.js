57be333340bcb2e81fb621a97c0ff0b6d664edb7
정준우 <woojunjung18@gmail.com>
2023-07-28 14:54:36 +0900

    Feat: 팬 피드 페이지 피드 생성 모달 형태만 생성

--- ourola/frontend/src/components/fanfeed/CreateFeedButton.js
+++ ourola/frontend/src/components/fanfeed/CreateFeedButton.js
@@ -1,12 +1,38 @@
-import React from 'react';
+// import React from 'react';
+// import '../../style/fanfeed/CreateFeedButton.css'
+
+// function CreateFeed () {
+//     return (
+//         <>
+//         <button className="Button-createfeed">당신의 아티스트에게 이야기를 건네보세요</button>
+//         </>
+//     )
+// }
+
+// export default CreateFeed;
+
+
+import React, {useState} from 'react';
+import FeedCreateModal from './FeedCreateModal'
 import '../../style/fanfeed/CreateFeedButton.css'
 
-function CreateFeed () {
-    return (
-        <>
-        <button className="Button-createfeed">당신의 아티스트에게 이야기를 건네보세요</button>
-        </>
-    )
-}
+const OpenFeedCreateModal = () => {
+    const [modalOpen, setModalOpen] = useState(false)
 
-export default CreateFeed;
+    const showModal = () => {
+        setModalOpen(true)
+    }
+    
+    return (
+          <div>
+            <button onClick={showModal} className="Button-createfeed">당신의 아티스트에게 이야기를 건네보세요</button>
+            {modalOpen && (
+              <FeedCreateModal
+                state={{ setModalOpen }}
+              ></FeedCreateModal>
+            )}
+          </div>
+      );
+    };
+
+export default OpenFeedCreateModal;
