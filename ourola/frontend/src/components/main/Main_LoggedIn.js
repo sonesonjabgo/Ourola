6023eda6998d70dd7aad126f5cca88eae831ed6e
ChangHo_Jin <65750442+jin-chang-ho@users.noreply.github.com>
2023-07-31 17:06:24 +0900

    Refactor : FE 파일 정리

--- ourola/frontend/src/components/main/Main_LoggedIn.js
+++ ourola/frontend/src/components/main/Main_LoggedIn.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import '../../style/main/Main_LoggedIn.css'
-import NewCards from '../cards/NewCards'
-import FollowingCards from '../cards/FollowingCards'
+import React from "react";
+import "../../style/main/Main_LoggedIn.css";
+import NewCards from "../common/cards/NewCards";
+import FollowingCards from "../common/cards/FollowingCards";
 
-function MainNoLoggedIn () {
-    return (
-        <>
-        <div className="mainbanner">
-            Ourola
-        </div>
-        <div className="artists-following">
-            나의 오로라
-        </div>
-        <div className="underline"></div>
-        <div className="card-list"><FollowingCards /></div>
-        <div className="artists-new">
-            당신의 오로라에 새로운 색을 더해보세요
-        </div>
-        <div className="underline"></div>
-        <div className="card-list"><NewCards /></div>
-        </>
-    )
+function MainNoLoggedIn() {
+  return (
+    <>
+      <div className="mainbanner">Ourola</div>
+      <div className="artists-following">나의 오로라</div>
+      <div className="underline"></div>
+      <div className="card-list">
+        <FollowingCards />
+      </div>
+      <div className="artists-new">당신의 오로라에 새로운 색을 더해보세요</div>
+      <div className="underline"></div>
+      <div className="card-list">
+        <NewCards />
+      </div>
+    </>
+  );
 }
 
-export default MainNoLoggedIn
+export default MainNoLoggedIn;
