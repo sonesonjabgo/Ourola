931e4bc2df3ed3fc3b1bcd613ac8fd00aae68f4e
ChangHo_Jin <65750442+jin-chang-ho@users.noreply.github.com>
2023-07-31 16:40:25 +0900

    Revert : 사라진 파일 되돌리기

--- ourola/frontend/src/components/fanfeed/FanFeed.js
+++ ourola/frontend/src/components/fanfeed/FanFeed.js
@@ -1,21 +1,38 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import HeaderLoggedIn from "../common/header/Header_LoggedIn";
-import ArtistPageMenu from "../common/header/ArtistPageMenu_FanFeed";
-import CreateFeedButton from "./CreateFeedButton";
-import AnnouncementOneline from "../others/announcement/AnnouncementOneline";
-import FanFeedProfile from "./FanFeedProfile";
-import FanFeedFeedList from "./FanFeedFeedList";
-import "../../style/fanfeed/FanFeed.css";
+import { Link } from 'react-router-dom'
+import Header from '../header/Header'
+import ArtistPageMenu from '../header/ArtistPageMenu_FanFeed'
+import CreateFeedButton from "./CreateFeedButton"
+import AnnouncementOneline from "../announcement/AnnouncementOneline"
+import FanFeedProfile from './FanFeedProfile'
+import FanFeedList from './FanFeedList'
+import '../../style/fanfeed/FanFeed.css'
 import { useLocation } from "react-router-dom";
 
-function Fanfeed() {
-  return (
-    <>
-      <div className="FanFeed-Header">
-        <div>
-          <HeaderLoggedIn />
+function Fanfeed () {
+
+    return(
+        <>
+        <div className="FanFeed-Header">
+            <div><Header /></div>
+            <div><ArtistPageMenu /></div>
+        </div>
+        <div className="Content-container">
+            <div className="Button-createfeed-container">
+                <CreateFeedButton />
+            </div>
+            <Link to='/announcement'>
+            <div className="OnelineAnnouncement-container">
+                <AnnouncementOneline />
+            </div>
+            </Link>
+            <div className='Fanfeed-profile-container'>
+                <FanFeedProfile />
+            </div>
+            <div className='Fanfeed-feed-container'>
+                <FanFeedList />
+            </div>
         </div>
         <div>
           <ArtistPageMenu />
