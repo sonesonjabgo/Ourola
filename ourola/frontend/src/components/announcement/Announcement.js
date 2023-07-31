52c2ee6391d39efa3244b2a6864c0216b01bb6ce
정준우 <woojunjung18@gmail.com>
2023-07-31 15:53:39 +0900

    Feat: 헤더 통합

--- ourola/frontend/src/components/announcement/Announcement.js
+++ ourola/frontend/src/components/announcement/Announcement.js
@@ -1,4 +1,4 @@
-import "../../style/announcement/Announcement.css";
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import AnnouncementList from "./AnnouncementList";
