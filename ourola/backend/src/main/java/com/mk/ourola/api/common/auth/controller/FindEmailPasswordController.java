92e1068fdb862085eed4c634a4458fa789e43ecb
최가연 <cn01803@naver.com>
2023-08-02 13:04:44 +0900

    Feat: 비밀번호 찾기 및 변경 기능 구현

--- ourola/backend/src/main/java/com/mk/ourola/api/common/auth/controller/FindEmailPasswordController.java
+++ ourola/backend/src/main/java/com/mk/ourola/api/common/auth/controller/FindEmailPasswordController.java
@@ -1,5 +1,7 @@
 package com.mk.ourola.api.common.auth.controller;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,4 +30,43 @@ public class FindEmailPasswordController {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
+	@PostMapping("/password")
+	public ResponseEntity<?> findPassword(@RequestBody Map<String, String> map) {
+		try {
+			System.out.println("find-controller: "+map.get("email"));
+			findEmailPasswordService.findPassword(map.get("email"));
+			return new ResponseEntity<>("토큰 발송 성공", HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	// 입력받은 토큰이 유효한지
+	// true: 유효함, false: 유효하지 않음
+	@PostMapping("/verify-token")
+	public ResponseEntity<?> verifyToken(@RequestBody Map<String, String> map) {
+		try {
+			System.out.println("find-controller: "+map.get("token"));
+
+			return new ResponseEntity<>(findEmailPasswordService.verifyToken(map.get("token")), HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	// 토큰 인증 성공 시 비밀번호 변경
+	@PostMapping("/modify-password")
+	public ResponseEntity<?> modifyPassword(@RequestBody Map<String, String> map) {
+		try {
+			findEmailPasswordService.modifyPassword(map.get("email"), map.get("newPassword"));
+			return new ResponseEntity<>("비밀번호 변경 성공", HttpStatus.OK);
+		} catch(Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
 }
