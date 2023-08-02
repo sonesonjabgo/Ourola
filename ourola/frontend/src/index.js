d169c304f2f54e308b32c2878072f55567a26921
Jaehyung <thswogud02@gmail.com>
2023-08-02 15:37:59 +0900

    Feat: 아이디 찾기 기능 추가

--- ourola/frontend/src/index.js
+++ ourola/frontend/src/index.js
@@ -16,7 +16,7 @@ import ShopItemDetail from "./components/shop/ShopItemDetail";
 import "./index.css";
 import Header from "./components/common/header/Header";
 import ArtistHeader from "./components/common/header/ArtistPageMenu";
-import Signup from "./components/auth/Singup";
+import Signup from "./components/auth/SignUp";
 
 import axios from "axios";
 import MyPage from "components/mypage/MyPage";
