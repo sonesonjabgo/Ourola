21cb02be0a72f445279e19dee743226fd2ae671e
Jaehyung <thswogud02@gmail.com>
2023-08-04 17:20:06 +0900

    Docs: 스프린트 회고

--- ourola/frontend/src/components/others/membershipOnly/MembershipContentBasic.js
+++ ourola/frontend/src/components/others/membershipOnly/MembershipContentBasic.js
@@ -1,18 +1,12 @@
 import "../../../style/others/membershipOnly/MembershipOnly.css";
 
-const MembershipContentBasic = ({ setModalOpen }) => {
-    // 모달 끄기
-    const closeModal = () => {
-        setModalOpen(false);
-        // console.log(modalOpen)
-    };
-
+const MembershipContentBasic = ({ title, closeModal }) => {
     return (
         <div className="modalContainer">
             <button onClick={closeModal}>
             X
             </button>
-            <h1>content</h1>
+            <h1>{title}</h1>
         </div>
     )
 }
