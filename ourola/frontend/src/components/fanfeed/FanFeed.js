7ab18494d494132bfef5186f056e3a3e203e9546
ChangHo_Jin <65750442+jin-chang-ho@users.noreply.github.com>
2023-08-07 17:07:00 +0900

    Style : CSS 정리

--- ourola/frontend/src/components/fanfeed/FanFeed.js
+++ ourola/frontend/src/components/fanfeed/FanFeed.js
@@ -10,49 +10,8 @@ import "../../style/fanfeed/FanFeed.css";
 import { useLocation } from "react-router-dom";
 
 function Fanfeed() {
-
   const pathname = window.location.pathname;
   const group = pathname.split("/")[1];
-
-  const [loadingFeed, setLodingFeed] = useState(true);
-  const [fanFeed, setFanFeed] = useState([]);
-  const [groupInfo, setGroupInfo] = useState();
-
-  const accessToken = localStorage.getItem("Authorization");
-
-  const config = {
-    headers: {
-      Authorization: "Bearer " + accessToken,
-      "Content-Type": "application/json",
-    },
-  };
-
-  useEffect(() => {
-    axios
-      .get(`search/${group}`, config)
-      .then((response) => {
-        setGroupInfo(response.data.groupInfo);
-        console.log(response.data)
-      })
-      .catch((error) => {
-        console.error("현재 접속 중인 페이지의 그룹 정보를 불러올 수 없어 이 글을 보고 계십니다", error);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    axios
-      .get(`/${group}/feed`, config)
-      .then((response) => {
-        setFanFeed(response.data);
-        setLodingFeed(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data : ", error);
-        setLodingFeed(false);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <>
@@ -60,20 +19,16 @@ function Fanfeed() {
         <div className="buttonCreatefeedContainer">
           <CreateFeedButton />
         </div>
-        <Link to="/announcement">
-          <div className="onelineAnnouncementContainer">
-            <AnnouncementOneline />
-          </div>
-        </Link>
-        <div className="fanfeedProfileContainer">
-          <FanFeedProfile groupInfo={groupInfo}/>
+        <div className="onelineAnnouncementContainer">
+          <Link to={"https://i9d204.p.ssafy.io/" + group + "/announcement"}>
+            <AnnouncementOneline group={group} />
+          </Link>
         </div>
-        <div id="group" className="fanfeedFeedContainer">
-          {!loadingFeed ? (
-              <FanFeedList group={group} fanFeed={fanFeed}/>
-          ) : (
-            <div></div>
-          )}
+        <div className="fanfeedProfileContainer">
+          <FanFeedProfile />
+        </div>
+        <div className="fanfeedFeedContainer">
+          <FanFeedList />
         </div>
       </div>
     </>
