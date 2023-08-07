7e88e72b8a25194f2ebba39e23e4b8a0e6145d22
정준우 <woojunjung18@gmail.com>
2023-08-07 14:58:11 +0900

    Feat: 그룹 카드별로 다른 페이지로 이동 후 해당 페이지에 맞는 피드만 출력

--- ourola/frontend/src/components/fanfeed/FanFeedProfile.js
+++ ourola/frontend/src/components/fanfeed/FanFeedProfile.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import '../../style/fanfeed/FanFeedProfile.css'
 
-function FanFeedProfile () {
+function FanFeedProfile (groupInfo) {
 
     const user = {
         name: 'PePe'
     }
 
-    const Group = {
-        name: 'SEVENTEEN'
-    }
-
+    console.log(groupInfo)
     return (
         <>
         <div className='fanFeedProfileGroupContainer'>
             <div className='fanFeedProfileGroupNamespace'>
                 {/* 임시로 지정, DB 연결 필요 */}
-                {Group.name}
+                
             </div>
         </div>
         {/* 마이페이지 구현 후 연결 필요 */}
