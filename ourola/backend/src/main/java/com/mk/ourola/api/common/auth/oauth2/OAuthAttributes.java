c48b50835558526678907dc49e7564352281703c
최가연 <cn01803@naver.com>
2023-08-08 12:59:16 +0900

    Refactor: oauth 로그인시에 닉네임 설정

--- ourola/backend/src/main/java/com/mk/ourola/api/common/auth/oauth2/OAuthAttributes.java
+++ ourola/backend/src/main/java/com/mk/ourola/api/common/auth/oauth2/OAuthAttributes.java
@@ -79,7 +79,7 @@ public class OAuthAttributes {
 			.socialType(socialType)
 			.socialId(oauth2UserInfo.getId())
 			.email(UUID.randomUUID() + "@socialUser.com")
-			// .nickname(oauth2UserInfo.getNickname())
+			.nickname(oauth2UserInfo.getNickname())
 			// .imageUrl(oauth2UserInfo.getImageUrl())
 			.role(Role.USER)
 			.build();
