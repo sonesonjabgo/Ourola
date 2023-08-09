5c87b080dc6f3891028d6de4312a6df846768f35
최영창 <choiyc1446@gmail.com>
2023-08-09 15:00:07 +0900

    Feat: 검색 결과 출력 기능

--- ourola/frontend/src/App.js
+++ ourola/frontend/src/App.js
@@ -1,51 +1,78 @@
-import React, { useState, useEffect } from "react";
-import { Link, Router, Routes, Route } from "react-router-dom";
-import Header from "./components/common/header/Header";
-import MainLoggedIn from "./components/main/MainLoggedIn";
-import MainNoLoggedIn from "./components/main/MainnoLoggedIn";
+  import React, { useState, useEffect } from "react";
+  import { Link, Router, Routes, Route, useLocation } from "react-router-dom";
+  import Header from "./components/common/header/Header";
+  import MainLoggedIn from "./components/main/MainLoggedIn";
+  import MainNoLoggedIn from "./components/main/MainnoLoggedIn";
 
-// artist 선언 방식 fix 필요
-function App() {
-  const artist = "BTS";
-  const [isLoggedIn, setLoggedIn] = useState(false);
 
-  // 로그인 상태 변경 함수
-  const handleLoginSuccess = () => {
-    setLoggedIn(true);
-  };
+  import AllCards from "./components/common/cards/AllCards";
+  import SearchResult from "./components/main/SearchResult";
 
-  // 로그아웃 상태 변경 함수
-  const handleLogout = () => {
-    setLoggedIn(false);
-  };
+  // artist 선언 방식 fix 필요
+  function App() {
+    const artist = "BTS";
+    const [isLoggedIn, setLoggedIn] = useState(false);
 
-  useEffect(() => {
-    // 로컬스토리지에서 Authorization 값을 가져옴
-    const authorization = localStorage.getItem("Authorization");
+    const {state} = useLocation();
+    console.log("state 받음: ", state);
 
-    // Authorization 값이 존재하면 로그인 상태로 설정
-    if (authorization) {
+    // 로그인 상태 변경 함수
+    const handleLoginSuccess = () => {
       setLoggedIn(true);
-    } else {
+    };
+
+    // 로그아웃 상태 변경 함수
+    const handleLogout = () => {
       setLoggedIn(false);
-    }
-  }, []);
+    };
 
-  return (
-    <div id="App" className="App">
-      <Header
-        isLoggedIn={isLoggedIn}
-        onLogin={handleLoginSuccess}
-        onLogout={handleLogout}
-      />
-      <div className="main"></div>
-      <div className="main">
-        {isLoggedIn ? <MainLoggedIn /> : <MainNoLoggedIn />}
+    useEffect(() => {
+      // 로컬스토리지에서 Authorization 값을 가져옴
+      const authorization = localStorage.getItem("Authorization");
+
+      // Authorization 값이 존재하면 로그인 상태로 설정
+      if (authorization) {
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+    }, []);
+
+    useEffect(() => {
+      // 페이지가 처음 로드될 때와 새로고침 시 state 값을 null로 설정
+      const resetState = () => {
+      if (state) {
+        window.history.replaceState({}, document.title, window.location.pathname);
+      }
+    };
+
+      window.addEventListener("beforeunload", resetState);
+
+      return () => {
+        window.removeEventListener("beforeunload", resetState);
+      };
+    }, [state]);
+
+    return (
+      <div id="App" className="App">
+        <Header
+          isLoggedIn={isLoggedIn}
+          onLogin={handleLoginSuccess}
+          onLogout={handleLogout}
+        />
+        <div className="main"></div>
+        <div className="main">
+          {isLoggedIn ? <MainLoggedIn /> : <MainNoLoggedIn />}
+        </div>
+
+        <div className="main">
+          {state ? <SearchResult allGroup={state} /> : null}
+        </div>
+
+        <Link to="/seventeen/online-concert/list">라이브</Link>
+
       </div>
+    );
+  }
 
-      <Link to="/seventeen/online-concert/list">라이브</Link>
-    </div>
-  );
-}
-
-export default App;
+  export default App;
