8fea3740cfa460a89da0b8d4f823604db837381f
김경숙 <lovablekks@naver.com>
2023-08-09 14:24:44 +0900

    Fix : 온라인 콘서트 프론트 수정

--- ourola/frontend/src/index.js
+++ ourola/frontend/src/index.js
@@ -31,9 +31,9 @@ import OnlineConcertView from "components/media/onlineconcert/OnlineConcertView"
 import Temp from "components/media/onlineconcert/temp";
 
 // EC2 서버
-// axios.defaults.baseURL = "https://i9d204.p.ssafy.io:8001";
+axios.defaults.baseURL = "https://i9d204.p.ssafy.io:8001";
 // 로컬
-axios.defaults.baseURL = "http://localhost:8000";
+// axios.defaults.baseURL = "http://localhost:8000";
 
 // axios.defaults.withCredentials = true;
 
