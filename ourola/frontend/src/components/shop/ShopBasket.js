c8c65fed6b4c80afb47f9463f6b68ebc4c44a215
정준우 <woojunjung18@gmail.com>
2023-08-09 17:49:51 +0900

    Docs: 230809 한 일 추가

--- ourola/frontend/src/components/shop/ShopBasket.js
+++ ourola/frontend/src/components/shop/ShopBasket.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import axios from 'axios'
+import '../../style/shop/ShopBasket.css'
 
 const ShopBasket = () => {
+    const location = useLocation()
+    
+    // 현재 로그인 중인 사용자의 장바구니 리스트 불러오기
+    const [allBasket, setAllBasket] = useState()
+
+    useEffect(() => {
+        axios.get()
+    })
+
     return (
         <>
+        <div className = "testing">하하</div>
         </>
     )}
 
