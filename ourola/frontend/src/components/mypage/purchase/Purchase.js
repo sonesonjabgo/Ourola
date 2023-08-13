7dc2db58f644735cbf9eb73db1351b1b0222aa0b
kyungsook <lovablekks@naver.com>
2023-08-14 06:31:30 +0900

    Feat : 마이페이지 프론트 구현

---
+++ ourola/frontend/src/components/mypage/purchase/Purchase.js
@@ -0,0 +1,21 @@
+import PurchaseHistory from "./PurchaseHistory";
+import "../../../style/mypage/purchase/Purchase.css";
+
+const Purchase = ({ config }) => {
+  return (
+    <div className="purchaseMain">
+      <div className="purchaseMainTitle">
+        <span className="titleText">구매 내역 </span>
+        <span className="sortBtnArea">
+          <button className="sortBtn membership">멤버십</button>
+          <button className="sortBtn onlineConcert">온라인 콘서트</button>
+        </span>
+      </div>
+      <div className="purchaseBody">
+        <PurchaseHistory config={config} />
+      </div>
+    </div>
+  );
+};
+
+export default Purchase;
