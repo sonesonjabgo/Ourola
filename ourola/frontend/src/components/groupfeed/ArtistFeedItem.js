f982ebd41bc986ca3bf6d401f29d76db078a39eb
정준우 <woojunjung18@gmail.com>
2023-08-16 17:22:15 +0900

    Fix: 장바구니 아이템 CSS 적용, 장바구니 페이지 내 뒤로가기 버튼 구현, 아티스트 피드 삭제 버튼 누락 해결

--- ourola/frontend/src/components/groupfeed/ArtistFeedItem.js
+++ ourola/frontend/src/components/groupfeed/ArtistFeedItem.js
@@ -9,6 +9,8 @@ import bookmarkfill from "../../assets/icons/bookmarkfill.png";
 import likeclick from "../../assets/icons/like.png";
 import notlikeclick from "../../assets/icons/notlike.png";
 import commentclick from "../../assets/icons/comment.png";
+import openedBin from "../../assets/icons/opened_bin.png";
+import closedBin from "../../assets/icons/closed_bin.png";
 
 const ArtistFeedItem = ({
   id,
@@ -192,6 +194,8 @@ const ArtistFeedItem = ({
       });
   };
 
+  const [openBin, setOpenBin] = useState(closedBin)
+
   return (
     <div id="artistFeedItem" className="artistFeedItem">
       <div id="aritstFeedHeader" className="aritstFeedHeader">
@@ -326,6 +330,9 @@ const ArtistFeedItem = ({
             </div>
           </div>
         </div>
+        {userInfo.id === artistId || userInfo.role === "CHANNEL_ADMIN" ? (
+              <img className="deleteBin" src={openBin} onClick={deleteRequest} onMouseOver={() => setOpenBin(openedBin)} onMouseOut={() => setOpenBin(closedBin)} />
+            ) : null}
       </div>
     </div>
   );
